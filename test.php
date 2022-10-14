2368c55bc4c1bc0920687f9a92f3ef697c8a9774
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-14 11:21:32 +0700

    Update

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
 
-echo '<style>body{white-space:pre-wrap}</style>';
-echo call_user_func("x\\linkify", file_get_contents(__DIR__ . D . 'test'));
+$content = file_get_contents(__DIR__ . D . 'test');
+
+echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . $content . '</pre>';
+echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . fire("x\\linkify", [$content], (object) []) . '</pre>';
 
 exit;
