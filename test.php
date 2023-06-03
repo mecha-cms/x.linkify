2208158438855a743192a849334b95a9fe59e708
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:32:55 +0700

    Update

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . $content . '</pre>';
-echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . fire("x\\linkify", [$content], (object) []) . '</pre>';
+echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . x\linkify($content) . '</pre>';
 
 exit;
